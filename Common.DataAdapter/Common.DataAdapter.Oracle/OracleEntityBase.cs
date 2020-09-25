1105f5b8eaf97f6e946c3f1287433fcec43c411c
Igor Boras <igor.boras@outlook.com>
2020-09-25 20:33:47 +0200

    Add Oracle adapter

---
+++ Common.DataAdapter/Common.DataAdapter.Oracle/OracleEntityBase.cs
@@ -0,0 +1,11 @@
+﻿using Common.DataAdapter.Library;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Common.DataAdapter.Oracle
+{
+    public class OracleEntityBase : EntityBase
+    {
+        [NotMapped]
+        public int RETURN_VALUE { get; set; }
+    }
+}
